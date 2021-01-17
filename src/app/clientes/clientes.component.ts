bdbf47bbf3e6807e87917051fea0184d42c04f62
Neri Laredo <nerila1310@gmail.com>
2021-01-17 12:50:02 -0600

    Primeros pasos con localStorage, getItem y SetItem

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Clientes{
+  nombre: String;
+  apellido: String;
+  edad: number;
+}
+
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
@@ -7,9 +13,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesComponent implements OnInit {
 
+  clientes: Array<Clientes> = new Array<Clientes>();
+
   constructor() { }
 
   ngOnInit(): void {
+  
+      // this.clientes.push(
+      //   { nombre: 'Diana', apellido: 'Pérez', edad: 22 },
+      //   {nombre: 'Diana', apellido: 'Alvarado', edad: 20}
+      // )
+  }
+
+  guardarCliente(){
+    localStorage.setItem("clientes", JSON.stringify(this.clientes))
+  }
+
+  leerClientes(){
+   this.clientes= JSON.parse(localStorage.getItem("clientes")) 
   }
 
 }
